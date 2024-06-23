84d84400c1d267f2dd5a01d18f2187d8127d4085
hideaki0604 <159912621+hideaki0604@users.noreply.github.com>
2024-06-23 21:40:28 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,19 +1,18 @@
-//3の倍数
-let num = 15;
+let num = Math.floor(Math.random() * 16);
 
-if((num % 3) === 0){
-    console.log('3の倍数です');
+// 先に3と5の倍数
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log('3と5の倍数です');
 }
-
-//５の倍数
-if((num % 5) === 0){
-    console.log('5の倍数です');
+// 3の倍数
+else if (num % 3 === 0) {
+  console.log('3の倍数です');
 }
-
-//３と５の倍数
-if((num % 3) === 0 && (num % 5) === 0){
-    console.log('3と5の倍数です');
-} else {
-    console.log(num);
+// 5の倍数
+else if (num % 5 === 0) {
+  console.log('5の倍数です');
 }
-
+// それ以外
+else {
+  console.log(num);
+}
