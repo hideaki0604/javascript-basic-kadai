58ac5a49c1b4dbeb1a9dae97f8a04ca206ea9906
hideaki0604 <159912621+hideaki0604@users.noreply.github.com>
2024-07-28 04:39:38 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+
+  setTimeout(() => {
+
+    const text = document.getElementById('text');
+
+    text.textContent = 'ボタンをクリックしました';
+
+  }, 2000);
+});
